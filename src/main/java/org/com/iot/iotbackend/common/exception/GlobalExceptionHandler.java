1917698d371ca85ab90f9bdd20c4ec5d597b98a9
Kormap <kodh10@gmail.com>
2025-03-18 13:07:01 +0900

    feature : 본인인증 메일 발송 및 검증로직 구현, 글로벌 예외처리 구현

--- src/main/java/org/com/iot/iotbackend/common/exception/GlobalExceptionHandler.java
+++ src/main/java/org/com/iot/iotbackend/common/exception/GlobalExceptionHandler.java
@@ -1,33 +1,45 @@
 package org.com.iot.iotbackend.common.exception;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+    Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
+    // RuntimeException 처리
     @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<ErrorResponse> handleRuntimeException(RuntimeException ex) {
-        ErrorResponse errorResponse = new ErrorResponse(
-                HttpStatus.BAD_REQUEST.value(),
-                ex.getMessage(),
-                LocalDateTime.now()
-        );
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+    public ResponseEntity<Map<String, Object>> handleRuntimeException(HttpServletRequest request, RuntimeException ex) {
+        Map<String, Object> metaData = new HashMap<>();
+        metaData.put("status", HttpStatus.BAD_REQUEST.value());
+        metaData.put("message", ex.getMessage());
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("metaData", metaData);
+
+        log.error(ex.getMessage(), ex);
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
     }
 
-    // 기타 예외 처리 (예: NullPointerException 등)
+    // 그 외 모든 Exception 처리
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponse> handleGeneralException(Exception ex) {
-        ErrorResponse errorResponse = new ErrorResponse(
-                HttpStatus.INTERNAL_SERVER_ERROR.value(),
-                "An unexpected error occurred",
-                LocalDateTime.now()
-        );
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+    public ResponseEntity<Map<String, Object>> handleGenericException(HttpServletRequest request, Exception ex) {
+        Map<String, Object> metaData = new HashMap<>();
+        metaData.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+        metaData.put("message", "Internal Server Error: " + ex.getMessage());
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("metaData", metaData);
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
     }
 }
