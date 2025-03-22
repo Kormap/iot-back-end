39059faae2b62983aa602b4f5f9d8848d250acb0
Donghyeok Ko <101923048+Kormap@users.noreply.github.com>
2025-03-22 16:37:20 +0900

    feature : 실시간 모니터링 차트 조회 로직 구현

--- src/main/java/org/com/iot/iotbackend/repository/SensorDataRepository.java
+++ src/main/java/org/com/iot/iotbackend/repository/SensorDataRepository.java
@@ -1,13 +1,25 @@
 package org.com.iot.iotbackend.repository;
 
+import org.com.iot.iotbackend.entity.User;
 import org.com.iot.iotbackend.entity.sensor.SensorData;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface SensorDataRepository extends JpaRepository<SensorData, Long> {
     @Modifying
     @Query("DELETE FROM SensorData sd WHERE sd.user.id = :userId")
     void deleteByUserId(@Param("userId") Long userId);
+
+    @Query("SELECT sd FROM SensorData sd WHERE sd.user.id = :userId AND sd.sensorType = :sensorType " +
+            "AND sd.measuredAt >= :startOfDay AND sd.measuredAt <= :endOfDay ORDER BY sd.id ASC")
+    List<SensorData> findSensorDataForToday(@Param("userId") Long userId,
+                                            @Param("sensorType") String sensorType,
+                                            @Param("startOfDay") LocalDateTime startOfDay,
+                                            @Param("endOfDay") LocalDateTime endOfDay);
+
 }
